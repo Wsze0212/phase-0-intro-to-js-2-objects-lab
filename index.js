65938f94345a757c6a056627c3d758fcd1c8b792
Wai Sze <waisze0212@yahoo.com>
2022-09-26 19:59:26 -0500

    commit tree 1c4f295b01f1bb0fc694e9e1d9af145bf75020fe

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    "name": "Alice",
+    "streetAddress": "12 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+  
+    return { ...object, ...{[key]: value}};
+    
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+
+    object[key] = value; 
+    return object;
+}
+
+function deleteFromEmployeeByKey(object, key, value) {
+    return { ...object, ...{[key]: value}};
+    delete object[key];
+    
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key, value) {
+   
+    delete object[key];
+    return object;
+}
